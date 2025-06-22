3e62d6894750d70d719a7ce8c1b949ba5bd84884
Mechthild Roelfing <mechthild@roelfing.de>
2025-06-22 09:29:27 +0200

    structure: create board overlay edit

--- js/board.js
+++ js/board.js
@@ -1,18 +1,19 @@
 // Achtung: hier die genaue ID ermitteln und als Parameter mitgeben
-function openOverlay()
-{
-    // Achtung: hier aktuellen index ermitteln
-    
+function openOverlay() {
     const overlay = document.getElementById("overlay");
-
-    // ändere hier die Logik auf .remove -d-none
-    // overlay.classList.remove("d-none");
-    overlay.classList.add("overlay--visible");
+    overlay.style.display = "flex";
     document.body.style.overflow = 'hidden';
 }
 
 function closeOverlay() {
     const overlay = document.getElementById("overlay");
-    overlay.classList.remove("overlay--visible");
+    overlay.classList.remove("d-none");
     document.body.style.overflow = '';
+}
+
+function openEditOverlay() {
+    const story = document.getElementById("story");
+    const edit = document.getElementById("edit");
+    story.classList.add("d-none");
+    edit.classList.remove("d-none");
 }
