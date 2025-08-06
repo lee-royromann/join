9a114dfefe8d0b93b7fc85778914fae631fc29ec
Mechthild Roelfing <mechthild@roelfing.de>
2025-08-06 20:50:30 +0200

    style: board mobile - design updates not finalized

--- js/board.js
+++ js/board.js
@@ -439,13 +439,8 @@ async function toggleSubtaskStatus(index, taskId) {
  * Function to set the selected task priority.
  * Resets all priority buttons and applies the specifically choosen priority css class to it.
  */
-<<<<<<< HEAD
-function setEditPriority(priority) {
-    resetEditPriorityButtons();
-=======
 function setPriorityEdit(priority) {
     resetPriorityButtonsEdit();
->>>>>>> bbea70e84a4be683dd5a6525e624ba85a79d2023
     let button = document.getElementById(`edit__btn-${priority}`);
     let icon = document.getElementById(`edit__btn-${priority}-icon`);
     button.classList.add(`edit__button-prio--${priority}`);
@@ -456,11 +451,7 @@ function setPriorityEdit(priority) {
 
 function setInitialTaskPriority(task) {
     const taskPriority = task.priority.toLowerCase();
-<<<<<<< HEAD
-    setEditPriority(taskPriority);
-=======
     setPriorityEdit(taskPriority);
->>>>>>> bbea70e84a4be683dd5a6525e624ba85a79d2023
 }
 
  
@@ -468,11 +459,7 @@ function setInitialTaskPriority(task) {
  * Function to reset all priority buttons to their default state.
  * Removes any applied priority-specific modifier classes from each button.
  */
-<<<<<<< HEAD
-function resetEditPriorityButtons() {
-=======
 function resetPriorityButtonsEdit() {
->>>>>>> bbea70e84a4be683dd5a6525e624ba85a79d2023
     const priorities = ['urgent', 'medium', 'low'];
 
     priorities.forEach(priority => {
